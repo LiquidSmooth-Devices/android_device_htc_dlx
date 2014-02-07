b2e0fe22e1981a9d1c1a8ec2a825674da7f543ce
Sean Hoyt <seanhoyt963@gmail.com>
2014-02-07 12:35:23 -0600

    Liquify

--- liquid.mk
+++ liquid.mk
@@ -5,18 +5,18 @@ TARGET_SCREEN_WIDTH := 1080
 # Release name
 PRODUCT_RELEASE_NAME := dlx
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/dlx/full_dlx.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dlx
-PRODUCT_NAME := cm_dlx
+PRODUCT_NAME := liquid_dlx
 PRODUCT_BRAND := Verizon
 PRODUCT_MODEL := HTC6435LVW
 PRODUCT_MANUFACTURER := HTC
